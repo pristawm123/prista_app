65280bcb15667f78723e0e10900da294419e05b6
Prista Wafiq M <pristawafiq4@gmail.com>
2024-05-23 20:29:35 +0700

    perubahan login

--- android/app/src/main/kotlin/com/example/prista_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/prista_app/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.example.prista_app
 
-import io.flutter.embedding.android.FlutterActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.prista_app.databinding.ActivityMainBinding
 
-class MainActivity: FlutterActivity()
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = "Hello, View Binding!"
+    }
+}
